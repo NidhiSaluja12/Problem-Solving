61eca793d2093c27a4ff9498fb25588d90f1b3a1
NidhiSaluja12 <84669505+NidhiSaluja12@users.noreply.github.com>
2021-06-11 17:31:40 +0530

    Create 2D arrays.py

---
+++ 2D arrays.py
@@ -0,0 +1,26 @@
+def hourglassSum(arr):
+    maxSum = -99
+    for i in range (4):
+        for j in range(4):
+            top = sum(arr[i][j:j+3])
+            middle = arr[i+1][j+1]
+            bottom = sum(arr[i+2][j:j+3])
+            hourGlassSum = top+middle+bottom
+            maxSum = max(hourGlassSum, maxSum)
+    return maxSum
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr = []
+
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = hourglassSum(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+

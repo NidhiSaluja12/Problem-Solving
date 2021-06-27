59c3f7ce24ab1e8ce539d4b79e0fdc54a03ac962
NidhiSaluja12 <84669505+NidhiSaluja12@users.noreply.github.com>
2021-06-27 13:04:07 +0530

    Create Array_sum.py

---
+++ Array_sum.py
@@ -0,0 +1,20 @@
+def simpleArraySum(ar):
+    sum1 = 0
+    for i in ar:
+        sum1+=i
+    return sum1
+
+        
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+

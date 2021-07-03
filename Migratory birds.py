8bf4e39ad8b27d487cbcd24e3b8a024fd8d2418b
NidhiSaluja12 <84669505+NidhiSaluja12@users.noreply.github.com>
2021-07-03 23:43:48 +0530

    Create Migratory birds.py

---
+++ Migratory birds.py
@@ -0,0 +1,27 @@
+def migratoryBirds(arr):
+    arr.sort()
+    max1 = 1
+    count = 1
+    for i in range(len(arr)):
+        if arr[i]==arr[i-1]:
+            count+=1
+        else:
+            count=1
+        if count>max1:
+            max1 = count
+            ans = arr[i]
+    return ans
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+

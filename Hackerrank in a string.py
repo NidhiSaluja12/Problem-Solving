3c9dc27f3555838459b677892fcf468c173f0285
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2021-07-20 11:45:44 +0530

    Update Hackerrank in a string.py

--- Hackerrank in a string.py
+++ Hackerrank in a string.py
@@ -1,17 +1,8 @@
-#!/bin/python3
-
 import math
 import os
 import random
 import re
 import sys
-
-#
-# Complete the 'hackerrankInString' function below.
-#
-# The function is expected to return a STRING.
-# The function accepts STRING s as parameter.
-#
 
 def hackerrankInString(s):
     hack = 'hackerrank'
@@ -24,8 +15,6 @@ def hackerrankInString(s):
             return "YES"
     
     return "NO"
-    # Write your code here
-
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 

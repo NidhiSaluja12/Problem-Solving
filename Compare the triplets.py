d8afb112f478828eafebb9794cc9d1cde43570b7
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2022-04-16 23:25:55 +0530

    Update Compare the triplets.py

--- Compare the triplets.py
+++ Compare the triplets.py
@@ -20,7 +20,6 @@ if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     a = list(map(int, input().rstrip().split()))
-
     b = list(map(int, input().rstrip().split()))
     result = compareTriplets(a, b)
     fptr.write(' '.join(map(str, result)))

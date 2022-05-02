9acbc8d8e8cd76c32c34004913e4982bce16b799
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2022-05-02 23:25:51 +0530

    Update Angry Professor.py

--- Angry Professor.py
+++ Angry Professor.py
@@ -23,7 +23,6 @@ if __name__ == '__main__':
     t = int(input().strip())
     for t_itr in range(t):
         first_multiple_input = input().rstrip().split()
-
         n = int(first_multiple_input[0])
 
         k = int(first_multiple_input[1])

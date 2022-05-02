6df8163c43c5f7deb708361d65509be5c314e5cd
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2022-05-02 23:26:03 +0530

    Update Angry Professor.py

--- Angry Professor.py
+++ Angry Professor.py
@@ -24,7 +24,6 @@ if __name__ == '__main__':
     for t_itr in range(t):
         first_multiple_input = input().rstrip().split()
         n = int(first_multiple_input[0])
-
         k = int(first_multiple_input[1])
 
         a = list(map(int, input().rstrip().split()))

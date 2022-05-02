64cd0b59e6bfb6073543565163b8f21d9855aa2a
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2022-05-02 23:26:12 +0530

    Update Angry Professor.py

--- Angry Professor.py
+++ Angry Professor.py
@@ -25,7 +25,6 @@ if __name__ == '__main__':
         first_multiple_input = input().rstrip().split()
         n = int(first_multiple_input[0])
         k = int(first_multiple_input[1])
-
         a = list(map(int, input().rstrip().split()))
 
         result = angryProfessor(k, a)
